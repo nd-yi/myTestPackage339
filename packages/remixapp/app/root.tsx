444596a52fb20cb4f31ef21156d48b7ce18cc3a9
Yi <yi.song@activenetwork.com>
2023-01-09 19:28:07 +0800

    update

--- packages/remixapp/app/root.tsx
+++ packages/remixapp/app/root.tsx
@@ -24,6 +24,7 @@ export default function App() {
       <body>
         <Outlet />
         <ScrollRestoration />
+        <div>test</div>
         <Scripts />
         <LiveReload />
       </body>
